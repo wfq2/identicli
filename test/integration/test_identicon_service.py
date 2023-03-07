192d225b44e86e2def74e986ce42c07463adf14a
Wyatt Queirolo <wyatt@mavrck.co>
2023-03-07 17:42:05 -0500

    adding tests

--- test/integration/test_identicon_service.py
+++ test/integration/test_identicon_service.py
@@ -11,3 +11,18 @@ class TestIdenticonService:
         new_service = IdenticonService.load()
         assert new_service._count == 1
         assert new_service._db['test_user']
+
+    def test_adding_user_identicon(self):
+        service = IdenticonService()
+        service.create_user_identicon("wyatt")
+        assert service._count == 1
+        assert service._db['wyatt']
+
+    def test_adding_many_user_identicons(self):
+        service = IdenticonService()
+        for i in range(20):
+            service.create_user_identicon(str(i))
+        assert service._count == 20
+        for i in range(20):
+            if str(i) not in service._db:
+                raise Exception(f"{i} not in test database")
